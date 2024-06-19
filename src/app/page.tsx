84580f281a4e0906e7c9b28b3b250234fb847050
Adebayo Temitope <tsadebayo@myseneca.ca>
2024-06-19 10:25:43 -0400

    layout skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
-  return (
-    <main>
+"use client";
 
+import DropBox from "@/components/dropBox";
+import { useState } from "react";
+
+export default function Home() {
+  const [preview, setPreview] = useState<string | null>(null);
+  
+  return (
+    <main className="h-dvh w-full flex flex-col items-center p-4">
+      <section className="w-2/5 p-4 mt-32">
+        <p className="text-xl uppercase font-bold text-center">grit<sup className="lowercase italic text-sm opacity-50">3000</sup></p>
+        <div className="mt-10">
+          <DropBox 
+            title="Upload audio" 
+            optional={false} 
+            acceptedFiles={{ audio: ["audio/*"] }} 
+            setPreview={setPreview}
+          />
+        </div>
+      </section>
     </main>
   );
 }
